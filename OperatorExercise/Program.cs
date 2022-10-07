c08fe6d00be890335d6e0f49aea18f64616ae7b2
tyler ambrose <blitzinc98@yahoo.com>
2022-10-06 22:52:23 -0400

    Operator Exercises

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace OperatorExercise
+﻿using System;
+using System.Transactions;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        var a = 17;
+        var b = 4;
+        var div = a / b;
+        var mod = a % b;
+
+        Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+
+        var r = 20;
+        var pi= Math.PI;
+        var areaOfCirlce = pi * (r * r);
+        Console.WriteLine($"The area of a cirlcle with the radius {r} is {areaOfCirlce}");
     }
 }
